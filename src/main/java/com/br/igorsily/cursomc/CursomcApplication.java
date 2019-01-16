3a75d13db5a2add8cb98f0ba665c07c4272a07f5
igor.estagiario <igor.estagiario@dpat2019gti01.cbmes.gov.br.local>
2019-01-16 10:54:19 -0200

    Produto e associacao muito para muitos

--- src/main/java/com/br/igorsily/cursomc/CursomcApplication.java
+++ src/main/java/com/br/igorsily/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.br.igorsily.cursomc.model.categoria.Categoria;
+import com.br.igorsily.cursomc.model.produto.Produto;
 import com.br.igorsily.cursomc.repository.categoria.CategoriaRepository;
+import com.br.igorsily.cursomc.repository.produto.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -26,8 +31,20 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria categoria = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
 
-		categoriaRepository.saveAll(Arrays.asList(categoria, categoria2));
+		Produto produto = new Produto(null, "Computador", 2000.00);
+		Produto produto2 = new Produto(null, "Impressora", 800.00);
+		Produto produto3 = new Produto(null, "Mouse", 80.00);
 
+		categoria.setProdutos(Arrays.asList(produto, produto2, produto3));
+		categoria2.setProdutos(Arrays.asList(produto2));
+
+		produto.setCategorias(Arrays.asList(categoria));
+		produto2.setCategorias(Arrays.asList(categoria, categoria2));
+		produto3.setCategorias(Arrays.asList(categoria));
+
+		categoriaRepository.saveAll(Arrays.asList(categoria, categoria2));
+		produtoRepository.saveAll(Arrays.asList(produto, produto2, produto3));
+		
 	}
 
 }
--- src/main/java/com/br/igorsily/cursomc/model/produto/Produto.java
+++ src/main/java/com/br/igorsily/cursomc/model/produto/Produto.java
@@ -1,4 +1,4 @@
-package com.br.igorsily.cursomc.model.categoria;
+package com.br.igorsily.cursomc.model.produto;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -8,12 +8,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-import com.br.igorsily.cursomc.model.produto.Produto;
+import com.br.igorsily.cursomc.model.categoria.Categoria;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -23,17 +25,20 @@ public class Categoria implements Serializable {
 
 	private String nome;
 
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	private Double preco;
 
-	public Categoria() {
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
 
+	public Produto() {
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -52,12 +57,20 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public Double getPreco() {
+		return preco;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	@Override
@@ -76,7 +89,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
