5face3b2ea3a2cfb48982e7a2c0a30e400b1d7f1
igor.estagiario <igor.estagiario@dpat2019gti01.cbmes.gov.br.local>
2019-01-16 10:29:16 -0200

    Criando operacao de instanciacao

--- src/main/java/com/br/igorsily/cursomc/CursomcApplication.java
+++ src/main/java/com/br/igorsily/cursomc/CursomcApplication.java
@@ -1,14 +1,33 @@
 package com.br.igorsily.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.br.igorsily.cursomc.model.categoria.Categoria;
+import com.br.igorsily.cursomc.repository.categoria.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
-}
+	@Override
+	public void run(String... args) throws Exception {
 
+		Categoria categoria = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(categoria, categoria2));
+
+	}
+
+}
