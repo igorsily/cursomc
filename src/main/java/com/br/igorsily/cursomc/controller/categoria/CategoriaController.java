bc389611ec6622439360a3dd9c9d19b149098967
igor.estagiario <igor.estagiario@dpat2019gti01.cbmes.gov.br.local>
2019-01-17 15:04:13 -0200

    inserindo uma nova Categoria com POST

--- src/main/java/com/br/igorsily/cursomc/controller/categoria/CategoriaController.java
+++ src/main/java/com/br/igorsily/cursomc/controller/categoria/CategoriaController.java
@@ -1,12 +1,15 @@
 package com.br.igorsily.cursomc.controller.categoria;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.br.igorsily.cursomc.model.categoria.Categoria;
 import com.br.igorsily.cursomc.service.categoria.CategoriaService;
@@ -21,9 +24,19 @@ public class CategoriaController {
 	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
 	public ResponseEntity<?> findById(@PathVariable Integer id) {
 
-		Categoria obj = categoriaService.findById(id);
+		Categoria categoria = categoriaService.findById(id);
 
-		return new ResponseEntity<>(obj, HttpStatus.OK);
+		return ResponseEntity.ok().body(categoria);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+
+		categoria = categoriaService.insert(categoria);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+
 	}
 
 }
