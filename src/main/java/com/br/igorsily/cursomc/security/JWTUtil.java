e79967ae24becf76f591dce353631fd334a0cab9
Igor Sily <igorsily2@gmail.com>
2019-01-22 23:58:11 -0200

    Implementando autorizacao

--- src/main/java/com/br/igorsily/cursomc/security/JWTUtil.java
+++ src/main/java/com/br/igorsily/cursomc/security/JWTUtil.java
@@ -3,9 +3,9 @@ package com.br.igorsily.cursomc.security;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.config.web.server.ServerHttpSecurity.OAuth2ResourceServerSpec.JwtSpec;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -24,4 +24,44 @@ public class JWTUtil {
 				.signWith(SignatureAlgorithm.HS512, secret.getBytes()).compact();
 
 	}
+
+	public boolean tokenValido(String token) {
+
+		Claims claims = getClaims(token);
+
+		if (claims != null) {
+			String username = claims.getSubject();
+			Date expiration = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+
+			if (username != null && expiration != null && now.before(expiration)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private Claims getClaims(String token) {
+
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public String getUsername(String token) {
+		Claims claims = getClaims(token);
+
+		if (claims != null) {
+			
+			String username = claims.getSubject();
+			
+			if (username != null) return username;
+
+		}
+
+		return null;
+	}
+
 }
